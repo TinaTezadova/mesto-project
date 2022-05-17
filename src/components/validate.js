b43d4e681d062cd3ae451b0c9d51b56843787b93
Sergey <veselovsa@list.ru>
2022-05-17 21:00:57 +0900

    feat:add class validate

--- src/components/validate.js
+++ src/components/validate.js
@@ -1,3 +1,73 @@
+export default class FormValidator {
+  constructor(data, selector) {
+    this._form = document.querySelector(selector);
+    this._inputErrorClass = data.inputErrorClass;
+    this._inputSelector = data.inputSelector;
+    this._errorClass = data.errorClass;
+    this._formInputs = Array.from(this._form.querySelectorAll(data.inputSelector));
+    this._formButton = this._form.querySelector(data.submitButtonSelector);
+    this._inactiveButtonClass = data.inactiveButtonClass;
+    this._errorFormFields = Array.from(this._form.querySelectorAll(data.errorFormFieldClass));
+  }
+  _enableFormButton() {
+    this._formButton.classList.remove(this._inactiveButtonClass);
+    this._formButton.disabled = false;
+  }
+  _disabledFormButton() {
+    this._formButton.classList.add(this._inactiveButtonClass);
+    this._formButton.disabled = true;
+  }
+  _toggleFormButton() {
+    if (this._formInputs.some(input => {
+        return input.validity.valid !== true
+      })) {
+      this._disabledFormButton()
+    } else {
+      this._enableFormButton()
+    }
+  }
+  _checkInputValidity(input, formError) {
+    if (!input.validity.valid) {
+      input.classList.add(this._inputErrorClass);
+      formError.textContent = input.validationMessage;
+      formError.classList.add(this._errorClass);
+    } else {
+      input.classList.remove(this._inputErrorClass);
+      formError.textContent = '';
+      formError.classList.remove(this._errorClass);
+    }
+  }
+  _handelnputValidation(input, formError) {
+    return () => {
+      this._checkInputValidity(input, formError)
+      this._toggleFormButton()
+    }
+  }
+  _setEventListeners(input, formError) {
+    input.addEventListener('input', this._handelnputValidation(input, formError))
+  }
+  enableValidation() {
+    this._formInputs.forEach(input => {
+      const formError = input.nextSibling.nextElementSibling
+      this._setEventListeners(input,formError)
+    })
+  }
+  resetForm() {
+    this._form.reset()
+    this._formInputs.forEach(input => {
+      input.classList.remove(this._inputErrorClass)
+    })
+    this._errorFormFields.forEach(item => {
+      item.textContent = '';
+    })
+    this._toggleFormButton()
+  }
+}
+
+
+
+
+
 const showError = (input, errorMessage, inputErrorClass, errorClass) => {
   input.classList.add(inputErrorClass);
   const formError = input.nextSibling.nextElementSibling
@@ -7,7 +77,7 @@ const showError = (input, errorMessage, inputErrorClass, errorClass) => {
 
 const hideError = (input, inputErrorClass, errorClass) => {
   input.classList.remove(inputErrorClass);
-  const formError = input?.nextSibling.nextElementSibling
+  const formError = input ? .nextSibling.nextElementSibling
   formError.classList.remove(errorClass);
   formError.textContent = '';
 };
