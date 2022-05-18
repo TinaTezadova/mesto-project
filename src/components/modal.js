372eeee623d60407b03f3dec9ca611deb2a55b09
Sergey <veselovsa@list.ru>
2022-05-18 21:09:54 +0900

    feat:added class Popup

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,6 +1,38 @@
 const popups = document.querySelectorAll('.popup');
 const photoViewierPopup = document.querySelector('#photo-viewier');
 
+export default class Popup {
+    constructor(selectorPopup) {
+    this._popup = document.querySelector(selectorPopup);
+    this._handleEscClose = this._handleEscClose.bind(this);
+    }
+    openPopup() {
+        this._popup.classList.add('popup_open');
+        document.addEventListener('keydown', this._handleEscClose);
+    }
+    closePopup() {
+        this._popup.classList.remove('popup_open');
+        document.removeEventListener('keydown', this._handleEscClose);
+    }
+    _handleEscClose(e) {
+        if (e.key === 'Escape') {
+            this.closePopup()
+        }
+    }
+    handleOverlayClick(e) {
+        if(e.target.classList.contains('popup_open')) {
+            this.closePopup()
+        }
+    }
+    setEventListeners() {
+        const button = this._popup.querySelector('.popup__button');
+        button.addEventListener('click', () => {
+            this.closePopup()
+        })
+        this._popup.addEventListener('click', this.handleOverlayClick.bind(this))
+    }
+}
+
 const closePopupWithEsc = (e) => {
     if (e.key === 'Escape') {
         const popupActive = document.querySelector('.popup_open');
